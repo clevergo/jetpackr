2b215c33c97c9d5674cf7d063cce5d0e70a35326
razonyang <razonyang@gmail.com>
2020-03-12 21:34:48 +0800

    Add unit test

--- loader_test.go
+++ loader_test.go
@@ -5,6 +5,8 @@
 package packrloader
 
 import (
+	"bytes"
+	"io/ioutil"
 	"reflect"
 	"testing"
 
@@ -16,5 +18,42 @@ func TestNew(t *testing.T) {
 	l := New(box)
 	if !reflect.DeepEqual(box, l.box) {
 		t.Errorf("expected box %v, got %v", box, l.box)
+	}
+}
+
+func TestLoaderExists(t *testing.T) {
+	box := packr.New("views", "./example/views")
+	l := New(box)
+	views := []string{"index.tmpl", "non-existence.tmpl"}
+	for _, name := range views {
+		actualName, actualExists := l.Exists(name)
+		expectedExists := box.Has(name)
+		if expectedExists != actualExists {
+			t.Errorf("expected exists %t, got %t", expectedExists, actualExists)
+		}
+		if actualName != name {
+			t.Errorf("expected name %s, got %s", name, actualName)
+		}
+	}
+}
+
+func TestLoaderOpen(t *testing.T) {
+	box := packr.New("views", "./example/views")
+	l := New(box)
+	views := []string{"index.tmpl", "non-existence.tmpl"}
+	for _, name := range views {
+		file, err := l.Open(name)
+		expectedFile, expectedErr := box.Open(name)
+		if expectedErr != nil {
+			if err == nil || err.Error() != expectedErr.Error() {
+				t.Errorf("expected error %s, got %s", expectedErr, err)
+			}
+			continue
+		}
+		content, _ := ioutil.ReadAll(file)
+		expectedContent, _ := ioutil.ReadAll(expectedFile)
+		if !bytes.Equal(content, expectedContent) {
+			t.Errorf("expected file content %s, got %s", content, expectedContent)
+		}
 	}
 }
